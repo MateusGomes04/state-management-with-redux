c74cfd84152523f18496d99a91a2cdbe6b95fa7b
mateus gomes <mateusgomesc04@gmail.com>
2022-11-17 12:51:14 -0300

    Invoking action creator #02

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,40 +1,36 @@
-import "./Intervalo.css"
-import React from "react"
-import Card from "./Card"
-import { connect } from "react-redux"
-
-import { alterarNumeroMinimo } from "../store/actions/numeros"
-
+import './Intervalo.css'
+import React from 'react'
+import { connect } from 'react-redux'
+import Card from './Card'
+import { alterarNumeroMinimo } from '../store/actions/numeros'
 function Intervalo(props) {
-    const { min, max } = props
-
- 
+    const {min, max} = props;
     return (
-        <Card title="Intervalo de Números" red>
-            <div className="Intervalo">
-                <span>
-                    <strong>Mínimo:</strong>
-                    <input type="number" value={min} readOnly></input>
-
-                </span>
-                <span>
-                    <strong>Máximo:</strong>
-                    <input type="number" value={max} readOnly></input>
-                </span>
+        <Card title="Intevalo de Números" red>
+            <div className='Intervalo'>
+            <span>
+                <strong>Mínimo:</strong>
+                
+                <input type="number" value={min} 
+                onChange={e => props.alterarMinimo(+e.target.value)} />
+            </span>
+            <span>
+                <strong>Máximo:</strong>
+                <input type="number" value={max} readOnly />
+            </span>
             </div>
-
         </Card>
     )
 }
-
-function mapStateToProp(state) {
+function mapStateToProps(state) {
     return {
         min: state.numeros.min,
-        max: state.numeros.max
+        max: state.numeros.max,
     }
 }
 
-function mapActionCreatorsToProp(dispatch) {
+
+function mapDispatchToProp(dispatch) {
     return {
         alterarMinimo(novoNumero) {
             // Action Creator -> Action
@@ -44,4 +40,4 @@ function mapActionCreatorsToProp(dispatch) {
     }
 }
 
-export default connect(mapStateToProp, mapActionCreatorsToProp) (Intervalo)
+export default connect(mapStateToProps, mapDispatchToProp) (Intervalo) 
